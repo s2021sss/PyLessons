a76ad385d59f1f261c881c15f2054b1a441fdd31
Sergei <99397935+s2021sss@users.noreply.github.com>
2022-03-06 22:08:43 +0300

    Update dz_07.03.22_2.py

--- start/dz_07.03.22/dz_07.03.22_2.py
+++ start/dz_07.03.22/dz_07.03.22_2.py
@@ -1,14 +1,13 @@
 import statistics
-lst=[2, 6, 8, 66, 12, 94, 57, 45, 3, 89, 34]
+lst=[94, 6, 8, 66, 12, 2, 95, 57, 45, 3, 89, 34]
 min_i=lst.index(min(lst))
 max_i=lst.index(max(lst))
 if (min_i+1<max_i):
     print (f"Среднее арифметическое: {statistics.mean(lst[min_i+1:max_i])} ")
-    mediana=statistics.median(lst[min_i+1:max_i])
-    for i in range(min_i+1, max_i):
+elif (max_i+1<min_i):
+    mediana=statistics.median(lst[max_i+1:min_i])
+    for i in range(max_i+1,min_i):
         lst[i] = mediana
     print (f"Измененный массив {lst}")
-elif (min_i+1==max_i):
+elif (min_i+1==max_i or min_i-11==max_i ):
     print ("Макс и мин числа находятся рядом ")
-else:
-    print ("Минимальное число находится после максимального")
